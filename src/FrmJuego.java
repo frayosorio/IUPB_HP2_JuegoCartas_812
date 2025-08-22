faa7c6a614d411f947d557c5d9eea54d051da8d2
fray osorio <frayosorio@gmail.com>
2025-08-22 13:26:02 -0500

    Se comienza lainterfaz grafica de usuario

---
+++ src/FrmJuego.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class FrmJuego extends JFrame {
+
+public FrmJuego(){
+    setTitle("Juguemos al apuntado");
+    setSize(500, 300);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
+
+}
